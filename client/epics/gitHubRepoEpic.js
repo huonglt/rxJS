9ca9a48e773aa4d9470100216eb221693ab62f71
huonglt <lthuongvn@yahoo.com>
2017-03-21 20:29:10 +1300

    rename function getResponseData to responseData

--- client/epics/gitHubRepoEpic.js
+++ client/epics/gitHubRepoEpic.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 
 const githubURL = (user) => 'https://api.github.com/users/' + user + '/repos';
 const repoRequest = (action) => axios.get(githubURL(action.payload.user));
-const getResponseData = (response) => response.data;
+const responseData = (response) => response.data;
 const getRepoNames = (data) => data.map(repo => repo.name);
 
 export const gitHubRepoEpic = (action$) =>
   action$.ofType(USER_REPOS_ACTION.type)
-    .mergeMap((action) => Rx.Observable.fromPromise(repoRequest(action))).map(getResponseData).map(data => {
+    .mergeMap((action) => Rx.Observable.fromPromise(repoRequest(action))).map(responseData).map(data => {
       const repoNames = data.map(repo => repo.name);
       return userRepoSuccessAction(repoNames);
     });
