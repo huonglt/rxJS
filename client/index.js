b2257815e7db267b3e2b0e30c140c792f00ed927
huonglt <lthuongvn@yahoo.com>
2017-03-22 14:04:37 +1300

    dispatch the cancel action. and dispatch the User repo action

--- client/index.js
+++ client/index.js
@@ -1,6 +1,11 @@
 import { configStore } from './redux/store.js';
-import { USER_REPOS_ACTION, PING_ACTION } from './redux/actions.js';
+import { USER_REPOS_ACTION, PING_ACTION, CANCEL_USER_REPOS_ACTION } from './redux/actions.js';
 
 const store = configStore();
 store.dispatch(PING_ACTION);
 store.dispatch(USER_REPOS_ACTION);
+setTimeout(() => store.dispatch(CANCEL_USER_REPOS_ACTION), 500);
+setTimeout(() => {
+  console.log('resending USER_REPOS_ACTION after cancellation');
+  store.dispatch(USER_REPOS_ACTION)
+}, 1000);
