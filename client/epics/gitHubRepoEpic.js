89798da02d1043933c4a175b12420f73180651ec
huonglt <lthuongvn@yahoo.com>
2017-03-22 14:04:08 +1300

    create a cancellable observable. The http request will be cancelled if the CANCEL action is distpached

--- client/epics/gitHubRepoEpic.js
+++ client/epics/gitHubRepoEpic.js
@@ -1,15 +1,18 @@
 import Rx from 'rxjs/Observable';
-import { USER_REPOS_ACTION, userRepoSuccessAction } from '../redux/actions.js';
+import { USER_REPOS_ACTION, CANCEL_USER_REPOS_ACTION, userRepoSuccessAction } from '../redux/actions.js';
 import axios from 'axios';
 
-const githubURL = (user) => 'https://api.github.com/users/' + user + '/repos';
-const repoRequest = (action) => axios.get(githubURL(action.payload.user));
-const responseData = (response) => response.data;
-const getRepoNames = (data) => data.map(repo => repo.name);
+const githubURL     = (user) => 'https://api.github.com/users/' + user + '/repos';
+const httpRequest   = (action) => axios.get(githubURL(action.user));
+const responseData  = (response) => response.data;
+const getRepoNames  = (data) => data.map(repo => repo.name);
 
+const obserable$ = (action) => Rx.Observable.fromPromise(httpRequest(action))
+                                          .map(responseData)
+                                          .map(data => {
+                                            const repoNames = data.map(repo => repo.name);
+                                            return userRepoSuccessAction(repoNames);
+                                          });
 export const gitHubRepoEpic = (action$) =>
   action$.ofType(USER_REPOS_ACTION.type)
-    .mergeMap((action) => Rx.Observable.fromPromise(repoRequest(action))).map(responseData).map(data => {
-      const repoNames = data.map(repo => repo.name);
-      return userRepoSuccessAction(repoNames);
-    });
+    .mergeMap(action => obserable$(action).takeUntil(action$.ofType(CANCEL_USER_REPOS_ACTION.type)));
