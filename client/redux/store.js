8a9fc6921b1bcff4f9dcd2bf36264743dc74ba11
huonglt <lthuongvn@yahoo.com>
2017-03-22 17:02:11 +1300

    set function to send http request as configurable dependency for the epic

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { reducers } from './reducers.js';
 import { createEpicMiddleware} from 'redux-observable';
-import { rootEpic } from '../epics/index.js'
+import { rootEpic } from '../epics/index.js';
+import axios from 'axios';
 /*
  * config the redux store with the redux-observable middleware
  */
 export const configStore = () => {
-  const epicMiddleware = createEpicMiddleware(rootEpic());
+  const epicDependencies = { dependencies: { httpGet: axios.get } };
+  const epicMiddleware = createEpicMiddleware(rootEpic(), epicDependencies);
   const store = createStore(reducers, applyMiddleware(epicMiddleware));
   return store;
 }
