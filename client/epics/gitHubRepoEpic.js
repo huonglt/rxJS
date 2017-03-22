81a470032e7cede433cdc26a99fcbc40047cf472
huonglt <lthuongvn@yahoo.com>
2017-03-22 17:03:15 +1300

    httpGet request function is passed as parameter to the epic

--- client/epics/gitHubRepoEpic.js
+++ client/epics/gitHubRepoEpic.js
@@ -1,18 +1,16 @@
 import Rx from 'rxjs/Observable';
 import { USER_REPOS_ACTION, CANCEL_USER_REPOS_ACTION, userRepoSuccessAction } from '../redux/actions.js';
-import axios from 'axios';
 
 const githubURL     = (user) => 'https://api.github.com/users/' + user + '/repos';
-const httpRequest   = (action) => axios.get(githubURL(action.user));
 const responseData  = (response) => response.data;
 const getRepoNames  = (data) => data.map(repo => repo.name);
 
-const obserable$ = (action) => Rx.Observable.fromPromise(httpRequest(action))
+const obserable$ = (action, httpGet) => Rx.Observable.fromPromise(httpGet(githubURL(action.user)))
                                           .map(responseData)
                                           .map(data => {
                                             const repoNames = data.map(repo => repo.name);
                                             return userRepoSuccessAction(repoNames);
                                           });
-export const gitHubRepoEpic = (action$) =>
+export const gitHubRepoEpic = (action$, store, { httpGet }) =>
   action$.ofType(USER_REPOS_ACTION.type)
-    .mergeMap(action => obserable$(action).takeUntil(action$.ofType(CANCEL_USER_REPOS_ACTION.type)));
+    .mergeMap(action => obserable$(action, httpGet).takeUntil(action$.ofType(CANCEL_USER_REPOS_ACTION.type)));
