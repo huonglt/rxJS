8dcd44f66e3e57ea423ba53e480d6803d48b15f0
huonglt <lthuongvn@yahoo.com>
2017-03-22 14:03:25 +1300

    add CANCEL_USER_REPOS_ACTION. refactor the payload object

--- client/redux/actions.js
+++ client/redux/actions.js
@@ -1,10 +1,9 @@
 export const PING_ACTION = { type: 'PING' };
 export const PONG_ACTION = { type: 'PONG' };
-export const USER_REPOS_ACTION = { type: 'USER_REPO', payload: { user: 'huonglt' }};
+export const USER_REPOS_ACTION = { type: 'USER_REPO', user: 'huonglt' };
+export const CANCEL_USER_REPOS_ACTION = { type: 'CANCEL_USER_REPO_REQUEST' };
 
 export const userRepoSuccessAction = (repoNames) => ({
     type: 'USER_REPO_SUCCESS_ACTION',
-    payload: {
-      repos: repoNames
-    }
+    repos: repoNames
 });
